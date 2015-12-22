46f4dd3f0815159ebc1fdfd8d83e6b08ae484f62
robbileifss <robert@leifsson.net>
2015-12-22 23:02:26 +0000

    initial add hopefully it will hit the right  repo

---
+++ src/LeifssonNet5/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿
+$(function () {
+    if ($.browser.mozilla) {
+        $("[id$=content]").addClass('font-effect-anaglyph');
+    } else {
+        $("[id$=content]").addClass('font-effect-stonewash');
+    }
+});
+
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-24028549-1', 'auto');
+ga('send', 'pageview');
+
+
+var appInsights = window.appInsights || function (config) {
+    function s(config) { t[config] = function () { var i = arguments; t.queue.push(function () { t[config].apply(t, i) }) } } var t = { config: config }, r = document, f = window, e = "script", o = r.createElement(e), i, u; for (o.src = config.url || "//az416426.vo.msecnd.net/scripts/a/ai.0.js", r.getElementsByTagName(e)[0].parentNode.appendChild(o), t.cookie = r.cookie, t.queue = [], i = ["Event", "Exception", "Metric", "PageView", "Trace"]; i.length;) s("track" + i.pop()); return config.disableExceptionTracking || (i = "onerror", s("_" + i), u = f[i], f[i] = function (config, r, f, e, o) { var s = u && u(config, r, f, e, o); return s !== !0 && t["_" + i](config, r, f, e, o), s }), t
+}({
+    instrumentationKey: "02d0ea64-ecb4-470b-b4e6-6b836cbd4fb6"
+});
+
+window.appInsights = appInsights;
+appInsights.trackPageView();
+

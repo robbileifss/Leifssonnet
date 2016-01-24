357fc00d25d70c2ee397f15869dfa7b6634ac50d
robbileifss <robert@leifsson.net>
2016-01-24 20:58:44 +0000

    fixup

--- LeifssonNetMvc/Controllers/HomeController.cs
+++ LeifssonNetMvc/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace LeifssonNetMvc.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "";
 
             return View();
         }
